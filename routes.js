3337343543842911f244573c79784868e4d36ed8
Sebastian Real <seba.real.a@gmail.com>
2014-12-09 15:42:43 -0300

    se corrige controllers
    TODO, separarlo a ficheros aparte

--- routes.js
+++ routes.js
@@ -1,23 +1,92 @@
-function Routes(server){
-    this.server = server;
-    this.registeredPaths = [];
+var database = database = require('./database.json');
+
+function indexController(req, res){
+    res.render('index',{
+        noticias: database.noticias,
+        obras: database.obras
+    });
 }
 
-Routes.prototype = {
-    constructor: Routes,
+function corporacionController(req, res){
+    res.render('corporacion');
+}
 
-    registerPath: function(path, fn){
-        this.registeredPaths.push({
-            path: path,
-            callback: fn
-        });
-    },
+function sobreController(req, res){
+    res.render('sobre');
+    console.log("muestra Git");
+}
 
-    makeRoutes: function(){
-        var _this = this;
+function visitaController(req, res){
+    res.render('visita',{
+        reglas: database.reglas
+    });
+}
 
-        this.registeredPaths.forEach(function(route){
-            _this.server.get(route.path, route.callback);
-        });
-    }
-};
+function obrasController(req, res){
+    res.render('obras',{
+        obras: database.obras
+    });
+}
+
+function obraController(req, res){
+    var obras = database.obras,
+        obra = null;
+
+    obra = obras.filter(function(o){
+        return o.slug = req.params.slug;
+    });
+
+    console.log('llegue a obra de un algo : ' + req.params.slug);
+
+    obra = obra[0] || {};
+    res.render('obra');
+}
+
+function comoLlegarController(req, res){
+    res.render('como_llegar');
+}
+
+function mapaController(req, res){
+    res.render('mapa');
+}
+
+function recorridoController(req, res){
+    res.render('recorrido');
+}
+
+function noticiasController(req, res){
+    res.render('noticias',{
+        noticias: database.noticias
+    });
+}
+
+function noticiaController(req, res){
+    var tmp = [];
+
+    tmp.push(database.noticias[0]);
+    tmp.push(database.noticias[1]);
+
+    res.render('noticia',{
+        noticias: tmp
+    });
+}
+
+function addRoutes(app){
+    app.get('/', indexController);
+    app.get('/corporacion', corporacionController);
+    app.get('/sobre', sobreController);
+    app.get('/visita', visitaController);
+    app.get('/obras', obrasController);
+    app.get('/obra/:slug', obraController);
+    app.get('/como-llegar', comoLlegarController);
+    app.get('/mapa', mapaController);
+    app.get('/recorrido', recorridoController);
+    app.get('/noticias', noticiasController);
+    app.get('/noticia', noticiaController);
+
+    app.get('*', function(req, res){
+        res.status(404).send('404, no está tu coso :(');
+    })
+}
+
+module.exports = addRoutes;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     http = require('http'),
     swig = require('swig'),
-    database = require('./database.json');
+    addRoutes = require('./routes.js');
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
@@ -12,78 +12,7 @@ app.set('view cache', false);
 
 swig.setDefaults({ cache : false });
 
-app.get('/', function(req, res){
-    res.render('index',{
-        noticias: database.noticias,
-        obras: database.obras
-    });
-});
-
-app.get('/corporacion', function(req, res){
-    res.render('corporacion');
-});
-
-app.get('/sobre', function(req, res){
-    res.render('sobre');
-    console.log("muestra Git");
-});
-
-app.get('/visita', function(req, res){
-    res.render('visita',{
-        reglas: database.reglas
-    });
-});
-
-app.get('/obras', function(req, res){
-    res.render('obras',{
-        obras: database.obras
-    });
-});
-
-app.get('/obra/:slug', function(req, res){
-    var obras = database.obras,
-        obra = null;
-
-    obra = obras.filter(function(o){
-        return o.slug = req.params.slug;
-    });
-
-    console.log('llegue a obra de un algo : ' + req.params.slug);
-
-    obra = obra[0] || {};
-    res.render('obra');
-});
-
-app.get('/como-llegar', function(req, res){
-    res.render('como_llegar');
-});
-
-app.get('/mapa', function(req, res){
-    res.render('mapa');
-});
-
-app.get('/recorrido', function(req, res){
-    res.render('recorrido');
-});
-
-app.get('/noticias', function(req, res){
-    res.render('noticias',{
-        noticias: database.noticias
-    });
-});
-
-app.get('/noticia', function(req, res){
-    var tmp = [];
-    tmp.push(database.noticias[0]);
-    tmp.push(database.noticias[1]);
-    res.render('noticia',{
-        noticias: tmp
-    });
-});
-
-app.get('*', function(req, res){
-    res.status(404).send('404, no está tu coso :(');
-})
+addRoutes(app);
 
 app.listen(3000, function(){
     console.log('estoy entero ready pos oshe');
