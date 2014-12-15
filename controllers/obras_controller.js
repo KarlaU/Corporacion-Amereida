c99d7d50efd64e9658998a87fe188717f1dc298d
Norman Th. Glaves <n.glaves@gmail.com>
2014-12-14 21:50:24 -0300

    implementacion de templates

--- controllers/obras_controller.js
+++ controllers/obras_controller.js
@@ -1,8 +1,16 @@
 var database = require('../database.json');
 
 function obrasController(req, res){
+    cols = database.obras;
+    c1 = cols.slice(0,3);
+    c2 = cols.slice(3,5);
+    c3 = cols.slice(6,8);
+
     res.render('obras',{
-        obras: database.obras
+        col1: c1,
+        col2: c2,
+        col3: c3,
+
     });
 }
 
